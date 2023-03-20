edf4a2c30ef9f23cc05829cc07eb874a96c3f27f
Febriany Renata <96133771+FebrianyRenata02@users.noreply.github.com>
2023-03-20 15:46:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 fetch('https://api.covid19api.com/summary')
     .then(response => response.json())
     .then(data => {
-        // const globalData = data.Global;
-        // const totalCases = globalData.TotalConfirmed;
-        // const totalDeaths = globalData.TotalDeaths;
-        // const totalRecovered = globalData.TotalRecovered;
-        // console.log(globalData)
+        const globalData = data.Global;
+        const totalCases = globalData.TotalConfirmed;
+        const totalDeaths = globalData.TotalDeaths;
+        const totalRecovered = globalData.TotalRecovered;
+        console.log(globalData)
+    
         console.log(data)
         console.log("date", data.Date)
 
         console.log(data)
         console.log("massage", data.Message)
 
-        // document.querySelector('#total').innerHTML = totalCases
-        // document.querySelector('#death').innerHTML = totalDeaths
-        // document.querySelector('#recovered').innerHTML = totalRecovered
+        document.querySelector('#total').innerHTML = totalCases
+        document.querySelector('#death').innerHTML = totalDeaths
+        document.querySelector('#recovered').innerHTML = totalRecovered
     })
     .catch(error => console.error(error));
